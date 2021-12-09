26094dfdaca7271bda387f9e4a2e0478ca8055be
weixinyuanyuan <94581273+weixinyuanyuan@users.noreply.github.com>
2021-12-09 13:06:08 +0800

    Update UserController.java

--- localGitpro1/src/com/nnxy/controller/UserController.java
+++ localGitpro1/src/com/nnxy/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.nnxy.controller;
 
 public class UserController {
-
+	public int number;
+	public String name;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		hello!
+		
 
 	}
 
